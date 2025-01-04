911e624c97079d2fbd3ca6815064e42fa01d9799
kaustubhai <kaustubh229@gmail.com>
2025-01-05 01:26:28 +0530

    feat: added name in review response

--- service/reviewController.js
+++ service/reviewController.js
@@ -1,32 +1,31 @@
 const { Op } = require('sequelize');
-const {Review, Appointment, ParameterLink, Parameter, ReviewInsight, FinalReview} = require("../model/index.model");
+const { Review, Appointment, ParameterLink, Parameter, ReviewInsight, FinalReview } = require("../model/index.model");
 const { structureResponse } = require('../utils/common.utils');
 
-class reviewController{
+class reviewController {
 
-      async checkReview(req,res){
-        try{
+    async checkReview(req, res) {
+        try {
             let reviews = await Review.findAll({
                 where: {
                     appointmentId: req.body.appointmentId
                 }
             })
-            console.log(reviews);
-            let response ;
-            if(reviews.length > 0 ){
+            let response;
+            if (reviews.length > 0) {
                 let reviewInsights = await ReviewInsight.findOne({
-                    where:{
+                    where: {
                         appointmentId: req.body.appointmentId
                     },
                 });
                 console.log("REVIEW INSIGHTS", reviewInsights)
                 let appointment = await Appointment.findOne({
-                    where:{
+                    where: {
                         id: req.body.appointmentId
                     }
                 })
                 let finalSavedReview = await FinalReview.findOne({
-                    where:{
+                    where: {
                         appointmentId: req.body.appointmentId
                     },
                 })
@@ -34,44 +33,45 @@ class reviewController{
                 let finalResponse = {
                     suggestivePricing: reviewInsights.dataValues.suggestivePricing,
                     rating: finalSavedReview.dataValues.finalRating,
-                    pricing:appointment.dataValues.pricing
+                    pricing: appointment.dataValues.pricing,
+                    firstName: appointment.dataValues.firstName,
+                    lastName: appointment.dataValues.lastName
                 }
                 response = structureResponse(finalResponse, 1, "Reviews Fetched successfully");
-            } else{
+            } else {
                 response = structureResponse(reviews, 1, "Reviews Fetched successfully");
             }
             return res.status(200).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
-        } catch(err){
+        } catch (err) {
             console.log(err);
             let response = structureResponse("Something Went Wrong", 0, "Failure")
             return res.status(500).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
         }
-      }
+    }
 
-      async getReviewParameters(req,res) {
-        try
-        {
-            let allParameters =[];
+    async getReviewParameters(req, res) {
+        try {
+            let allParameters = [];
             let parametersLink = await ParameterLink.findAll({
                 where: {
                     appointmentId: req.params.appointmentId
                 }
             });
-            for(let i=0;i<parametersLink.length;i++){
+            for (let i = 0; i < parametersLink.length; i++) {
                 let parameter = await Parameter.findOne({
-                    where:{
+                    where: {
                         id: parametersLink[i].dataValues.parameterId
                     }
                 })
                 allParameters.push({
-                    parameterId:parametersLink[i].dataValues.parameterId,
-                    parameterName:  parameter.dataValues.paramterName,
+                    parameterId: parametersLink[i].dataValues.parameterId,
+                    parameterName: parameter.dataValues.paramterName,
                     appointmentId: req.params.appointmentId,
                     description: parameter.dataValues.description
                 });
@@ -79,63 +79,62 @@ class reviewController{
             let response = structureResponse(allParameters, 1, "Reviews Parameter Fetched successfully");
             return res.status(200).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
-        } catch(err){
+        } catch (err) {
             console.log(err);
             let response = structureResponse("Something Went Wrong", 0, "Failure")
             return res.status(500).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
         }
-      }
+    }
 
-      async saveReviewRatings(req,res) {
-        try
-        {
+    async saveReviewRatings(req, res) {
+        try {
             let savedReviews = []
-            for(let i=0;i<req.body.ratings.length;i++) {
+            for (let i = 0; i < req.body.ratings.length; i++) {
                 let savedReview = await Review.create({
-                    appointmentId:req.body.appointmentId,
+                    appointmentId: req.body.appointmentId,
                     parameterId: req.body.ratings[i].parameterId,
                     rating: req.body.ratings[i].value,
                 })
                 savedReviews.push(savedReview);
             }
-            console.log("Saved the ratings for appointmentID ",req.body.appointmentId)
+            console.log("Saved the ratings for appointmentID ", req.body.appointmentId)
 
-            let ans = await this.fetchReviewAndSuggestivePricing(savedReviews,req.body.appointmentId)
-            
+            let ans = await this.fetchReviewAndSuggestivePricing(savedReviews, req.body.appointmentId)
+
             let response = structureResponse(ans, 1, "Reviews Parameter Fetched successfully");
             return res.status(200).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
-        } catch(err){
+        } catch (err) {
             console.log(err);
             let response = structureResponse("Something Went Wrong", 0, "Failure")
             return res.status(500).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
         }
-      }
+    }
 
-      async fetchReviewAndSuggestivePricing(reviews,appointmentId){
+    async fetchReviewAndSuggestivePricing(reviews, appointmentId) {
         console.log(reviews);
         let totalReviews = reviews.length;
         let totalRating = 0;
         let suggestPricing = 0;
-        for(let i=0;i<reviews.length;i++){
-            totalRating+=reviews[i].dataValues.rating
+        for (let i = 0; i < reviews.length; i++) {
+            totalRating += reviews[i].dataValues.rating
         }
-        let finalRating = totalRating/totalReviews;
-        if(finalRating>0 && finalRating < 2){
+        let finalRating = totalRating / totalReviews;
+        if (finalRating > 0 && finalRating < 2) {
             suggestPricing = 0;
-        } else if(finalRating>=2 && finalRating< 4){
+        } else if (finalRating >= 2 && finalRating < 4) {
             suggestPricing = 1;
-        } else{
+        } else {
             suggestPricing = 2;
         }
         let reviewInsights = await ReviewInsight.create({
@@ -149,18 +148,20 @@ class reviewController{
             finalRating: finalRating
         })
         let appointment = await Appointment.findOne({
-            where:{
+            where: {
                 id: appointmentId
             }
         })
-        console.log("Saved suggestive pricing ",reviewInsights)
+        console.log("Saved suggestive pricing ", reviewInsights)
         let finalResponse = {
             suggestivePricing: suggestPricing,
             rating: finalRating,
-            pricing:appointment.dataValues.pricing
+            pricing: appointment.dataValues.pricing,
+            firstName: appointment.dataValues.firstName,
+            lastName: appointment.dataValues.lastName
         }
         return finalResponse;
-      }
+    }
 }
 
 module.exports = new reviewController();
