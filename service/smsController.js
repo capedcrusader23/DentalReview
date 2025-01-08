7a384eb745f2400775ebd489516224ecb7f6439a
kaustubhai <kaustubh229@gmail.com>
2025-01-09 02:04:52 +0530

    fix: only pushing string plans in db

--- service/smsController.js
+++ service/smsController.js
@@ -2,32 +2,32 @@ const { Op } = require('sequelize');
 const twilio = require('twilio');
 const config = require('config');
 const accountSid = config.get("twillio").get("accountId");  // from your Twilio console
-const authToken  = config.get("twillio").get("authToken");     // from your Twilio console
-const {Appointment, Review} = require("../model/index.model");
+const authToken = config.get("twillio").get("authToken");     // from your Twilio console
+const { Appointment, Review } = require("../model/index.model");
 const { firstName, lastName, isPaid, phoneNo } = require('../model/appointment.model');
 const { timestamp } = require('../model/parameters.model');
 const { appointmentId } = require('../model/review.model');
 const client = twilio(accountSid, authToken);
 const SendSmsValidation = require("../validation/SendSmsValidation");
 const { structureResponse } = require('../utils/common.utils');
-const {InvalidPropertiesException} = require("../utils/exceptions/validation.exception")
+const { InvalidPropertiesException } = require("../utils/exceptions/validation.exception")
 
-class smsController{
-      async sendSms(req,res){
-        try{
+class smsController {
+    async sendSms(req, res) {
+        try {
             console.log(req.body)
             let validator = new SendSmsValidation(req.body);
             validator.validate();
             let createdAppointment = await Appointment.create({
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
-                pricing: req.body.pricing,
-                // pricing: JSON.stringify(req.body.pricing),
+                // pricing: req.body.pricing,
+                pricing: JSON.stringify(req.body.pricing),
                 isPaid: false,
                 phoneNo: req.body.phoneNo,
                 timestamp: new Date(),
             });
-            console.log("Record created with id ",createdAppointment.dataValues.id)
+            console.log("Record created with id ", createdAppointment.dataValues.id)
             // const message = await client.messages.create({
             //     body: "Thank you for choosing us. Please go through this link to complete payment: "+"https://dentalcare.com/payment/"+createdAppointment.dataValues.id,
             //     from: "+12314473531",  
@@ -38,45 +38,45 @@ class smsController{
             console.log(response)
             return res.status(200).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
-        } catch(err){
+        } catch (err) {
             console.log(err);
             let response;
-            if(err instanceof InvalidPropertiesException){
-                response = structureResponse(err.data, 0, "Failure") 
-            } else{
+            if (err instanceof InvalidPropertiesException) {
+                response = structureResponse(err.data, 0, "Failure")
+            } else {
                 response = structureResponse("Something Went Wrong", 0, "Failure")
             }
             return res.status(500).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
         }
-     }
+    }
 
-     async getAppointments(req,res){
-            try{
-                let appointments = await Appointment.findAll();
-                let response = structureResponse(appointments, 1, "Appointments Fetched");
+    async getAppointments(req, res) {
+        try {
+            let appointments = await Appointment.findAll();
+            let response = structureResponse(appointments, 1, "Appointments Fetched");
             return res.status(200).json({
                 message: response.headers.message,
-                data:response.body,
+                data: response.body,
             })
-            } catch(err){
-                console.log(err);
-                let response;
-                if(err instanceof InvalidPropertiesException){
-                    response = structureResponse(err.data, 0, "Failure") 
-                } else{
-                    response = structureResponse("Something Went Wrong", 0, "Failure")
-                }
-                return res.status(500).json({
-                    message: response.headers.message,
-                    data:response.body,
-                })
+        } catch (err) {
+            console.log(err);
+            let response;
+            if (err instanceof InvalidPropertiesException) {
+                response = structureResponse(err.data, 0, "Failure")
+            } else {
+                response = structureResponse("Something Went Wrong", 0, "Failure")
             }
-     }
+            return res.status(500).json({
+                message: response.headers.message,
+                data: response.body,
+            })
+        }
+    }
 }
 
 module.exports = new smsController();
